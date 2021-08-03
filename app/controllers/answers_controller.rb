0657efa663e70b314854e1e49313454061bc8f38
Exequiel Muñoz <eamunooz@gmail.com>
2021-08-03 12:52:57 -0400

    Stupid Coaching

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,13 @@
+class AnswersController < ApplicationController
+  def answer
+    @answer = begin
+      if params[:question] == "I am going to work"
+        "Great!"
+      elsif params[:question].ends_with?("?") 
+        "Silly question, get dressed and go to work!"
+      else
+        "I don't care, get dressed and go to work!"
+      end
+    end
+  end
+end
